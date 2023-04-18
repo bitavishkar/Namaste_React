443c810be2157e05265af80b13414161e187bcf0
Avishkar Ghadge <ghadgeavishkar@gmail.com>
2023-04-18 13:48:52 +0530

    learnt more about parcel

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
- 
+ import React from "react";
+ import  ReactDOM from "react-dom";
 
-//  const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h2",{},"i am a silbllbing ")   ,React.createElement("h1",{},"I am an H1 tag")]));
+ //  const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h2",{},"i am a silbllbing ")   ,React.createElement("h1",{},"I am an H1 tag")]));
 
 const grandpa = React.createElement("div",{class:"grandpa"},[React.createElement("div",{class:parent},[React.createElement("h1",{class:"child1"},"Hello this is the first child"),React.createElement("h1",{class:"child2"},"Hello this is the second child"),React.createElement("h1",{class:"child3"},"Hello this is the first child")]),React.createElement("div",{class:"parent"},React.createElement("h2",{class:"child4"},"LMAO BAS KAR"))]);
 
